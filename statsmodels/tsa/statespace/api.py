92e24b9739d1c900cad3ea317cf5a05d41eaf40b
冷愁 <yuchen.lyc@alibaba-inc.com>
2016-04-19 15:35:19 +0800

    Add Tbats Model and tools to statespace api file

--- statsmodels/tsa/statespace/api.py
+++ statsmodels/tsa/statespace/api.py
@@ -1,3 +1,4 @@
 from .sarimax import SARIMAX
 from .mlemodel import MLEModel, MLEResults
+from .tbats import TBATSModel, TBATSResults, seasonal_fourier_k_select_ic, tbats_k_order_select_ic
 from . import tools
